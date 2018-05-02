eeb65335da64b8eaf3b0e94a1aacbfff6b826aa4
seokhan <seok.han.io@gmail.com>
2018-05-02 20:48:43 +0900

    4장

--- tdd_in_js/__tests__/test_money.js
+++ tdd_in_js/__tests__/test_money.js
@@ -22,6 +22,9 @@ describe('TDD', () => {
     test('equality', () => {
         expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
         expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+
+        expect(new Franc(5).equals(new Dollar(6))).toBeTruthy();
     });
+
 });
 
