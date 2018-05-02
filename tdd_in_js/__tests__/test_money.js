7f0e0e2e9452ea3218d90efbd49e0b5b50b19292
seokhan <seok.han@bringprice.com>
2018-05-02 20:37:45 +0900

    4장

---
+++ tdd_in_js/__tests__/test_money.js
@@ -0,0 +1,27 @@
+var Dollar = function(amount) {
+    this.amount = amount
+
+    this.times = function(multiplier) {
+        return new Dollar(this.amount * multiplier);
+    }
+
+    this.equals = function(dollar) {
+        return this.amount == dollar.amount;
+    }
+
+};
+
+describe('TDD', () => {
+
+    test('multiplication', () => {
+        let five = new Dollar(5);
+        expect(new Dollar(10).equals(five.times(2))).toBeTruthy();
+        expect(new Dollar(15).equals(five.times(3))).toBeTruthy();
+    });
+
+    test('equality', () => {
+        expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+        expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+    });
+});
+
